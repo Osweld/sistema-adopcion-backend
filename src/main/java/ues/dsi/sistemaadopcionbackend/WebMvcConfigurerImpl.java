4c87d733387aee08516f2593779c4c8303382045
German <oswaldo101.gr@gmail.com>
2023-09-26 10:31:54 -0600

    Cambios en la configuracion de CORS

--- src/main/java/ues/dsi/sistemaadopcionbackend/WebMvcConfigurerImpl.java
+++ src/main/java/ues/dsi/sistemaadopcionbackend/WebMvcConfigurerImpl.java
@@ -12,7 +12,7 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://dsi-frontend.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 
